7c37cfb8e42d4477b5a740acf3d74ada833252a0
Hafsa Egal <hafsaegal915@gmail.com>
2025-03-20 22:02:12 +0300

    1st try

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// //DOM render functions
+// function renderOnePub(pub){
+//     const pubBar = document.getElementById('pub-bar')
+// }
+
+
+
+// //initial render
+// //get data and render our animals to the DOM
+
+// function initialize(){
+//     pupsData.forEach(pub =>renderOnePub(pub))
+// }
